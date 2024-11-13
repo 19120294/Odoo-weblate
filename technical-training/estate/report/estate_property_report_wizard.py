36f503dc0b6dfbb78a8964f98be7beadeee3e33c
19120294 <19120294@student.hcmus.edu.vn>
2024-11-13 13:30:34 +0700

    update code lesson 8

--- technical-training/estate/report/estate_property_report_wizard.py
+++ technical-training/estate/report/estate_property_report_wizard.py
@@ -12,26 +12,11 @@ class EstatePropertyReportWizard(models.TransientModel):
 
     def action_export_xlsx(self):
         # Kiểm tra ngày bắt đầu và ngày kết thúc
-        # if self.start_date > self.end_date:
-        #     raise UserError("Start Date must be before End Date!")
+        if self.start_date > self.end_date:
+            raise UserError("Start Date must be before End Date!")
 
-        # start_date = self.start_date
-        # end_date = self.end_date
-        # # buyer_id = self.buyer_id.id if self.buyer_id else None  
-        # # buyer_ids = ','.join(map(str, self.buyer_id.ids))
-        # buyer_ids = ','.join(map(str, self.buyer_ids.ids)) if self.buyer_ids else ''
-
-        # if buyer_ids:
-        #     url = f'/estate/property_report_xlsx?start_date={start_date}&end_date={end_date}&buyer_id={buyer_ids}'
-        # else:
-        #     url = f'/estate/property_report_xlsx?start_date={start_date}&end_date={end_date}'
-
-        # return {
-        #     'type': 'ir.actions.act_url',
-        #     'url': url,
-        #     'target': 'self',  
-        # }
-        # Gọi controller để xuất báo cáo
+        start_date = self.start_date
+        end_date = self.end_date
         buyer_ids = ','.join(map(str, self.buyer_ids.ids)) if self.buyer_ids else ''
         return {
             'type': 'ir.actions.act_url',
